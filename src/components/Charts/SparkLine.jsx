b7c7c2290c98bdc514c436c89f37c9468f77d0cb
Fadloovich <arabea169@gmail.com>
2023-02-21 22:42:13 +0200

    Completed Home Page

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-
-const SparkLine = () => {
-  return <div>SparkLine</div>;
+import {
+  SparklineComponent,
+  Inject,
+  SparklineTooltip,
+} from "@syncfusion/ej2-react-charts";
+const SparkLine = ({ id, color, height, width, data, type, currentColor }) => {
+  return (
+    <SparklineComponent
+      id="sparkline"
+      height="100px"
+      width="70%"
+      dataSource={data}
+      xName="xval"
+      yName="yval"
+      type="Line"
+    ></SparklineComponent>
+  );
 };
 
 export default SparkLine;
